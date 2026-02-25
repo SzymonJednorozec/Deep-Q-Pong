57bfb0f996e08b60a101d5711f100090a5bed99d
Jakub <jakub.konecki9@gmail.com>
2026-02-25 18:04:01 +0100

    feat: add basic game loop and ball class

--- game.py
+++ game.py
@@ -4,8 +4,9 @@ from pygame import Vector2 as Vec2
 pygame.init()
 font = pygame.font.Font('arial.ttf', 25)
 
-SPEED = 40
+SPEED = 100
 
+PADDLE_SPEED = 300
 
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
@@ -18,15 +19,18 @@ class PongGame:
         self.display = pygame.display.set_mode((self.w,self.h))
         self.clock = pygame.time.Clock()
         self.running = True
-        self.paddle_l = Paddle(Vec2(self.w-self.paddle_offset,self.h/2),self.display)
+        self.paddle_l = Paddle(Vec2(0+self.paddle_offset,self.h/2),self.display)
+        self.paddle_r = Paddle(Vec2(self.w-self.paddle_offset,self.h/2),self.display)
         # self.start()  TODO
     
     def _draw(self):
         self.display.fill(BLACK)
         self.paddle_l.draw()
+        self.paddle_r.draw()
         pygame.display.flip()
 
     def game_loop(self):
+        dt = 0
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -34,14 +38,17 @@ class PongGame:
                     quit()
 
             self._draw()
+            self._move(dt)
 
-            self.clock.tick(SPEED)
+            dt = self.clock.tick(SPEED)/1000
 
-
+    def _move(self,dt):
+        self.paddle_l.move_l(dt)
+        self.paddle_r.move_r(dt)
 
 
 class Paddle:
-    def __init__(self, pos: Vec2, display, width=20,height=80):
+    def __init__(self, pos: Vec2, display, width=20,height=100):
         self.pos = pos
         self.display = display
         self.w = width
@@ -50,6 +57,30 @@ class Paddle:
     def draw(self):
         pygame.draw.rect(self.display,WHITE,pygame.Rect(self.pos.x - (self.w/2),self.pos.y-(self.h/2),self.w,self.h))
 
+    def move_l(self,dt,):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.pos.y -= PADDLE_SPEED * dt
+        if keys[pygame.K_s]:
+           self.pos.y += PADDLE_SPEED * dt
+        
+    def move_r(self,dt,):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.pos.y -= PADDLE_SPEED * dt
+        if keys[pygame.K_DOWN]:
+           self.pos.y += PADDLE_SPEED * dt
+
+class Ball:
+    def __init__(self,pos,radius,display):
+        self.pos=pos
+        self.r=radius
+        self.display=display
+
+    def draw(self):
+        pygame.draw.circle(self.display,WHITE,self.pos,self.r)
+    
+    def move(self):
 
 if __name__ == "__main__":
     game = PongGame()
