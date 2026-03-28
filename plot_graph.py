bb48d656723fc6aa9ef126019939b6ef03c395e9
Jakub <jakub.konecki9@gmail.com>
2026-03-28 14:48:15 +0100

    feat: generate and save plots to png durning pause, include all instances

--- plot_graph.py
+++ plot_graph.py
@@ -20,3 +20,31 @@ def plot(scores, mean_scores,epsilons):
     plt.text(len(epsilons)-1, epsilons[-1], str(epsilons[-1]))
     plt.show(block=False)
     plt.pause(.1)
+
+
+def generate_and_save_plots(plot_data_list, epsilons, save_path="model/training_plot.png"):
+    num_instances = len(plot_data_list)
+    
+    fig, axes = plt.subplots(num_instances + 1, 1, figsize=(10, 4 * (num_instances + 1)))
+
+    for i, info in enumerate(plot_data_list):
+        ax = axes[i]
+        ax.set_title(f'Instance {i+1} Training')
+        ax.set_ylabel('Score')
+        ax.plot(info.scores, label='Score', color='blue')
+        ax.plot(info.mean_scores, label='Mean Score', color='orange')
+        ax.legend()
+
+        if info.scores:
+            ax.text(len(info.scores)-1, info.scores[-1], str(info.scores[-1]))
+            ax.text(len(info.mean_scores)-1, info.mean_scores[-1], f"{info.mean_scores[-1]:.2f}")
+
+    ax_eps = axes[-1]
+    ax_eps.set_title('Global Epsilon Decay')
+    ax_eps.set_xlabel('Total Games Finished (Across all instances)')
+    ax_eps.set_ylabel('Epsilon')
+    ax_eps.plot(epsilons, color='red')
+    
+    plt.tight_layout()
+    plt.savefig(save_path)
+    plt.close(fig) 
